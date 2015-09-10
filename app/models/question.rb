01321fc35be34427f0ebdc9b6eeca40235634e40
App Academy Student <student@appacademy.io>
2015-09-10 12:04:23 -0700

    Add several associations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,29 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :integer          not null, primary key
+#  text       :string
+#  poll_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Question < ActiveRecord::Base
   validates :poll_id, presence: true
   validates :text, presence: true
+
+  has_many :answer_choices,
+    class_name: "AnswerChoice",
+    foreign_key: :question_id,
+    primary_key: :id
+
+  belongs_to :poll,
+    class_name: "Poll",
+    foreign_key: :question_id,
+    primary_key: :id
+
+  has_many :responses,
+    through: :answer_choices,
+    source: :responses
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,4 +1,35 @@
+# == Schema Information
+#
+# Table name: responses
+#
+#  id               :integer          not null, primary key
+#  user_id          :integer
+#  answer_choice_id :integer
+#  created_at       :datetime
+#  updated_at       :datetime
+#
+
 class Response < ActiveRecord::Base
   validates :user_id, presence: true
   validates :answer_choice_id, presence: true
+
+  belongs_to :answer_choice,
+    class_name: "AnswerChoice",
+    foreign_key: :answer_choice_id,
+    primary_key: :id
+
+  belongs_to :respondent,
+    class_name: "User",
+    foreign_key: :user_id,
+    primary_key: :id
+
+  has_one :question,
+    through: :answer_choice,
+    source: :question
+
+  has_many :sibling_responses,
+    # Proc.new{ where 'Response.id != ?', id: }, 
+    through: :question,
+    source: :responses
+
 end
